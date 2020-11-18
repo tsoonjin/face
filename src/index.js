fdc9e7406aeb22ad1717857b742e7a5ae87103d4
jinified <jinified@gmail.com>
2020-11-18 23:09:13 +0800

    Working server

--- src/index.js
+++ src/index.js
@@ -7,6 +7,84 @@ const wsServer = new server({
   httpServer
 });
 
+const findPeers = (id, connection, code) => {
+  const room = peersByCode[code]
+  if (room) return room.filter(peer => peer.id !== id)
+  peersByCode[code] = [{ connection, id }];
+  return null
+}
+
+const handleIceCandidate = (id, connection, data) => {
+  const { code, candidate, type } = data
+  const peers = findPeers(id, connection, code)
+  if (peers) {
+    const peer = peers[0]
+    console.log(`${id} sending candidate to ${peer.id}`)
+    peer.connection.send(JSON.stringify({
+      type,
+      candidate,
+      id
+    }))
+  }
+}
+
+const handleOffer = (id, connection, data) => {
+  const { code, offer, type } = data
+  const peers = findPeers(id, connection, code)
+  if (peers) {
+    const peer = peers[0]
+    console.log(`${id} sending offer to ${peer.id}`)
+    peer.connection.send(JSON.stringify({
+      type,
+      offer,
+      id
+    }))
+  }
+}
+
+const handleAnswer = (id, connection, data) => {
+  const { code, answer, type } = data
+  const peers = findPeers(id, connection, code)
+  if (peers) {
+    const peer = peers[0]
+    console.log(`${id} sending answer to ${peer.id}`)
+    peer.connection.send(JSON.stringify({
+      type,
+      answer,
+      id
+    }))
+  }
+}
+
+const handleJoin = (id, connection, data) => {
+    const { code } = data
+    if (!peersByCode[code]) {
+      console.log(`New room: ${code} has been created by ${id}`)
+      peersByCode[code] = [{ connection, id }];
+    } else if (!peersByCode[code].find(peer => peer.id === id )) {
+      peersByCode[code].push({ connection, id });
+      console.log(`${id} has joined the room: ${code}`);
+    }
+}
+
+const handleLeave = (id, connection, data) => {
+  peersByCode[data.code] = peersByCode[data.code].filter(peer => peer.id !== id)
+  console.log(`${id} has left the room: ${data.code}`)
+}
+
+const handleChat = (id, connection, data) => {
+  const { text, code, type} = data
+  const peers = findPeers(id, connection, code)
+  if (peers) {
+    peers.forEach(peer => peer.connection.send(JSON.stringify({
+      id,
+      type,
+      code,
+      text
+    })));
+  }
+}
+
 // WebSocket
 
 const peersByCode = {};
@@ -16,34 +94,57 @@ wsServer.on("request", request => {
   const id = request.resourceURL.path.replace('/', '')
 
   connection.on('message', message => {
-    const { code } = JSON.parse(message.utf8Data);
-    if (!peersByCode[code]) {
-      console.log("Empty Room")
-      peersByCode[code] = [{ connection, id }];
-    } else if (!peersByCode[code].find(peer => peer.id === id )) {
-      peersByCode[code].push({ connection, id });
-      console.log("Found buddy")
+    let data;
+
+    //accepting only JSON messages
+    try {
+      data = JSON.parse(message.utf8Data);
+    } catch (e) {
+      console.log("Invalid JSON");
+      data = {};
     }
 
-    const peer = peersByCode[code].find(peer => peer.id !== id)
-    if (peer) {
-        console.log("Sending msg to buddy", peer.id)
-        peer.connection.send(message.utf8Data);
+    const { code, type } = data
+
+    console.log(`Origin ID: ${id}, Type: ${type}, Room: ${code}`)
+    switch (type) {
+      case 'join':
+        handleJoin(id, connection, data);
+        break;
+      case 'leave':
+        handleLeave(id, connection, data);
+        break;
+      case 'offer':
+        console.log(data.offer)
+        handleOffer(id, connection, data)
+        break;
+      case 'answer':
+        handleAnswer(id, connection, data)
+        break;
+      case 'candidate':
+        handleIceCandidate(id, connection, data)
+        break;
+      case 'chat':
+        handleChat(id, connection, data)
+        break;
+      default:
+        break
     }
+
   });
 
-    connection.on("close", () => {
-        const room = Object.values(peersByCode).filter(room => room.filter(peer => peer.id == id ))[0]
-        const peer = room.find(peer => peer.id !== id)
-        if (peer) {
-            peer.connection.send(
-                JSON.stringify({
-                    client: id,
-                    text: "I disconnected"
-                })
-            )
-        }
-    });
+  connection.on("close", () => {
+    const room = Object.values(peersByCode).filter(room => room.filter(peer => peer.id == id ))[0]
+    const peer = room.find(peer => peer.id !== id)
+    if (peer) {
+      peer.connection.send(
+        JSON.stringify({
+          client: id,
+          text: "I disconnected"
+        })
+      )
+    }
+  });
 });
 
 
